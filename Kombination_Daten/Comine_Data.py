b044b1b736b2fb2091339ce6b590c8705fdfd5b3
sttivand <timo.vandenwyenbergh@student.hs-anhalt.de>
2022-05-02 14:48:12 +0200

    #6 find_path_nextcloud einbinden

--- Kombination_Daten/Comine_Data.py
+++ Kombination_Daten/Comine_Data.py
@@ -6,21 +6,7 @@ from spectral import envi
 
 
 # Functions
-def find_path_nextcloud():
-    # Bestimmung des aktuellen Pfads
-    current_path = os.path.abspath(os.getcwd())
-
-    # Der Benutzername der Endgeräte unterscheidet sich
-    # Festlegen des Pfads der Nextcloud in Abhängigkeit vom Benutzername
-    if 'fgrassxx' in current_path:
-        path_nextcloud_fg = open('../Daten_einlesen/path_nextcloud_fgrassxx.txt')
-        path_nextcloud = path_nextcloud_fg.read()
-
-    elif 'timo' in current_path:
-        path_nextcloud_timo = open('../Daten_einlesen/path_nextcloud_timo.txt')
-        path_nextcloud = path_nextcloud_timo.read()
-
-    return path_nextcloud
+from Daten_einlesen.find_path_nextcloud import find_path_nextcloud
 
 print('Anfang')
 
