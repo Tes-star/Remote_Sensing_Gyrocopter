796a117a16f19bd842be31b5077938639a729166
Felix Graß <felix.grass@student.hs-anhalt.de>
2022-05-23 22:12:04 +0200

    #1 first results for presentation

---
+++ Datenexploration/find_path_nextcloud.py
@@ -0,0 +1,24 @@
+# Packages
+import os
+
+
+def find_path_nextcloud():
+    # Bestimmung des aktuellen Pfads
+    current_path = os.path.abspath(os.getcwd())
+    path_nextcloud = None
+
+    # Der Benutzername der Endgeräte unterscheidet sich
+    # Festlegen des Pfads der Nextcloud in Abhängigkeit vom Benutzername
+    if 'fgrassxx' in current_path:
+        path_nextcloud_fg = open('../Daten_einlesen/path_nextcloud_fgrassxx.txt')
+        path_nextcloud = path_nextcloud_fg.read()
+
+    elif 'Timo' in current_path:
+        path_nextcloud_timo = open('path_nextcloud_timo.txt')
+        path_nextcloud = path_nextcloud_timo.read()
+
+    if 'vdwti' in current_path:
+        path_nextcloud_timo = open('../Daten_einlesen/path_nextcloud_vdwti.txt')
+        path_nextcloud = path_nextcloud_timo.read()
+
+    return path_nextcloud
