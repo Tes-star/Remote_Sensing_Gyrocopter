6f167cecc1f15efd315bdcf5fc94fba31f51f610
sttivand <timo.vandenwyenbergh@student.hs-anhalt.de>
2022-05-31 16:55:05 +0200

    try

--- Modelling/Baselines/Data_Preprocessing.py
+++ Modelling/Baselines/Data_Preprocessing.py
@@ -1,10 +1,10 @@
 # Import Modules
 import os
-from glob import glob
+
+import numpy as np
 import spectral.io.envi as envi
 from sklearn.preprocessing import StandardScaler
-from spectral import open_image
-#from Code.find_path_nextcloud import find_path_nextcloud
+
 from Code.find_path_nextcloud import find_path_nextcloud
 
 
@@ -43,10 +43,47 @@ def import_labeled_data():
                 Y.append(data[row][column][117])
     return X, Y
 
+def import_labeled_photos():
+    # Pfad Nextcloud bestimmen
+
+    path_nextcloud = find_path_nextcloud()
+
+    # Festlegung, welches Grid zusammengelegt werden soll
+    windowsize_r = 200
+    windowsize_c = 200
+
+    # Bestimmung annotation_folder
+    grid_folder = path_nextcloud + "Daten_Gyrocopter/Oldenburg/Teilbilder/grid_" + str(windowsize_r) + "_" + str(
+        windowsize_c)
+    labeled_folder = grid_folder + '/labeled/'
+
+    # Liste aller Dateien in annotation_folder erstellen
+    files = os.listdir(labeled_folder)
+
+    # Aus Liste files .hdr Dateien löschen
+    for file in files:
+        if not file.endswith('.dat'):
+            files.remove(file)
+    X = []
+    Y = []
+    # labeled Bilder erstellen
+    for filename in files:
+        path_dat = labeled_folder + os.path.splitext(filename)[0] + '.dat'
+        path_hdr = labeled_folder + os.path.splitext(filename)[0] + '.hdr'
+        img = envi.open(file=path_hdr, image=path_dat)
+        data = img.open_memmap(writable=False)
+        dataholder=np.ndarray(shape=(200,200,118))
+        #for row in range(0, data.shape[0]):
+        #    for column in range(0, data.shape[1]):
+        dataholder[:][:][:]=data[:][:][:]
+
+        X.append(dataholder[:,:,0:109])
+        Y.append(dataholder[:,:,117])
+    return X, Y
 
 def scale_data(data):
     scaler = StandardScaler().fit(data)
     data = scaler.transform(data)
     return data
 
-X, Y = import_labeled_data()
+X, Y = import_labeled_photos()
