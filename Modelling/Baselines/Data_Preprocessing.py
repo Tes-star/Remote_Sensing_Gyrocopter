8ca5cc88689ba821e72c75d48fde23df92542b50
sttivand <timo.vandenwyenbergh@student.hs-anhalt.de>
2022-06-14 12:35:21 +0200

    #10

--- Modelling/Baselines/Data_Preprocessing.py
+++ Modelling/Baselines/Data_Preprocessing.py
@@ -54,6 +54,7 @@ def import_labeled_photos():
     windowsize_c = 200
 
     # Bestimmung annotation_folder
+    Oldenburg_folder = path_nextcloud + "Daten_Gyrocopter/Oldenburg/"
     grid_folder = path_nextcloud + "Daten_Gyrocopter/Oldenburg/Teilbilder/grid_" + str(windowsize_r) + "_" + str(
         windowsize_c)
     labeled_folder = grid_folder + '/labeled/'
@@ -71,21 +72,40 @@ def import_labeled_photos():
     X = []
     Y = []
     # labeled Bilder erstellen
+    #
+    path_dat = Oldenburg_folder + 'Oldenburg_combined_HSI_THERMAL_DOM.dat'
+    path_hdr = Oldenburg_folder + 'Oldenburg_combined_HSI_THERMAL_DOM.hdr'
+    img = envi.open(file=path_hdr, image=path_dat)
+    data = img.open_memmap(writable=False)
+    max_value = np.zeros(108)
+    for i in range(108):
+        max_value[i] = data[:, :, i].max()
+
     for filename in files:
         path_dat = labeled_folder + os.path.splitext(filename)[0] + '.dat'
         path_hdr = labeled_folder + os.path.splitext(filename)[0] + '.hdr'
         img = envi.open(file=path_hdr, image=path_dat)
         data = img.open_memmap(writable=False)
         # dataholder = np.ndarray(shape=(200, 200, 110))
-        dataholder = np.zeros((224, 224, 110))
+        dataholder = np.zeros((224, 224, 119), dtype=float)
         # for row in range(0, data.shape[0]):
         #    for column in range(0, data.shape[1]):
-        dataholder[0:200,0:200,:] = data[:][:][:]
+        dataholder[0:200, 0:200, 0:110] = data[:, :, 0:110]
 
-        # X.append(dataholder[0:200, 0:200, 0:108])
-        # Y.append(dataholder[0:200, 0:200, 109])
+        # reduce values between 0 and 1
+        for i in range(108):
+            dataholder[0:200, 0:200, i] = dataholder[0:200, 0:200, i] / max_value[i]
+
+        for row in range(0, 200):
+            for column in range(0, 200):
+                label_int = int(dataholder[row, column, 109]) + 110
+                dataholder[row, column, label_int] = 1
+        # dataholder[np.where(dataholder == 0)] = -1
+
         X.append(dataholder[:, :, 0:108])
-        Y.append(dataholder[:, :, 109])
+        Y.append(dataholder[:, :, 110:118])
+        # np.unique(dataholder[0:200,0:200,109], return_counts=True)
+        # dict(zip(unique, counts))
     return X, Y
 
 
@@ -95,4 +115,5 @@ def scale_data(data):
     return data
 
 
-X, Y = import_labeled_photos()
+if __name__ == '__main__':
+    X, Y = import_labeled_photos()
--- Modelling/Timo CNN/CNN.py
+++ Modelling/Timo CNN/CNN.py
@@ -1,55 +1,79 @@
-from segmentation_models import Unet
-import wandb
-from wandb.keras import WandbCallback
+import segmentation_models
 import tensorflow as tf
-from Modelling.Baselines.Data_Preprocessing import import_labeled_data, import_labeled_photos
-import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-# login
-wandb.init(project="NN_for_pixels", entity="pds_project", name='segmentation_models_Unet')
-wandb.init(config={})
+def cnn_segmentation_models(epochs, batch_size, optim, learning_rate, metrics, loss, name, backbone_name, activation):
+    from segmentation_models import Unet
+    import wandb
+    from wandb.keras import WandbCallback
 
+    from Modelling.Baselines.Data_Preprocessing import import_labeled_data, import_labeled_photos
+    import os
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    # login
+    wandb.init(project="cnn_segmentation_models", entity="pds_project", name='segmentation_models_Unet', reinit=True,
+               dir=r"C:\Users\vdwti\PycharmProjects\pds_gyrocopter\Modelling\Timo CNN")
+    wandb.config.update({"epochs": epochs, "batch_size": batch_size, 'metrics': metrics, 'loss': loss, 'optim': optim,
+                         'learning_rate': learning_rate,
+                         'backbone_name': backbone_name,
+                         'activation': activation})
+
+    # import Data
+    x, y = import_labeled_photos()
+    x_train_data = x[4:]
+    y_train_data = y[4:]
+    x_test_data = x[0:4]
+    y_test_data = y[0:4]
+
+    # Transofrm Data to tensor
+    x_train = tf.convert_to_tensor(x_train_data, dtype=tf.float32)
+    y_train = tf.convert_to_tensor(y_train_data, dtype=tf.float32)
+    x_test = tf.convert_to_tensor(x_test_data, dtype=tf.float32)
+    y_test = tf.convert_to_tensor(y_test_data, dtype=tf.float32)
+
+    # define number of channels
+    N = x_train.shape[-1]
+
+    # define model
+    model = Unet(backbone_name=backbone_name, encoder_weights=None, input_shape=(None, None, N), activation=activation,
+                 classes=8)
+
+    model.summary()
+    model.compile(optim, loss=loss, metrics=metrics)
+
+    model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(x_test, y_test),
+        callbacks=[WandbCallback()]
+    )
+    # wandb.finish()
+
+
+if __name__ == '__main__':
+    epochs = 5000
+    batch_size = 2
+    activation = 'softmax'
+    metrics = ['categorical_accuracy']  # categorical_accuracy
+    loss = tf.keras.losses.CategoricalHinge()
+    # loss=segmentation_models_pytorch.losses.constants.MULTICLASS_MODE
+    name = 'segmentation_models_Unet_V1'
+    learning_rate = 0.001
+    optim = tf.keras.optimizers.Adam(learning_rate)
+    backbone_name = 'resnet34'
+
+    cnn_segmentation_models(epochs, batch_size, optim, learning_rate, metrics, loss, name, backbone_name,activation)
+
+"""
 # Configuration
-epochs = 1000
-batch_size = 5
-metrics = ['accuracy']
-loss = tf.keras.losses.CategoricalCrossentropy()
-name = 'segmentation_models_Unet_V1'
-wandb.config.update({"epochs": epochs, "batch_size": batch_size,'metrics':metrics,'loss':loss})
-
-
-# import Data
-x, y = import_labeled_photos()
-x_train_data = x[0:10]
-y_train_data = y[0:10]
-x_test_data = x[10:]
-y_test_data = y[10:]
-
-# Transofrm Data to tensor
-x_train = tf.convert_to_tensor(x_train_data, dtype=tf.float32)
-y_train = tf.convert_to_tensor(y_train_data, dtype=tf.float32)
-x_test = tf.convert_to_tensor(x_test_data, dtype=tf.float32)
-y_test = tf.convert_to_tensor(y_test_data, dtype=tf.float32)
-
-# define number of channels
-N = x_train.shape[-1]
-
-# define model
-model = Unet(backbone_name='resnet34', encoder_weights=None, input_shape=(None, None, N), activation='sigmoid')
-
-model.summary()
-model.compile('Adam', loss=loss, metrics=metrics)
-
-wandb.init(project="NN_for_pixels", entity="pds_project",
-           name=name)
-model.fit(
-    x=x_train,
-    y=y_train,
-    batch_size=batch_size,
-    epochs=epochs,
-    validation_data=(x_test, y_test),
-    callbacks=[WandbCallback()],
-)
-wandb.finish()
+    epochs = 1000
+    batch_size = 5
+    metrics = ['accuracy']
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    name = 'segmentation_models_Unet_V1'
+    wandb.config.update({"epochs": epochs, "batch_size": batch_size,'metrics':metrics,'loss':loss})
+"""
